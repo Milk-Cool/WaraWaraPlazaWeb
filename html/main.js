f77073572a5b75cbfa239656b5a536ec38e8e88b
Milk-Cool <milan-pl@mail.ru>
2024-07-12 16:27:25 +0300

    cube

--- html/main.js
+++ html/main.js
@@ -6,3 +6,17 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+const animate = () => {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+	renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
