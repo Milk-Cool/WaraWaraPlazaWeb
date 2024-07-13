5481d3c4cf9de950c892d35072df5ab41e863e58
Milk-Cool <milan-pl@mail.ru>
2024-07-13 09:27:32 +0300

    rounded icons

--- html/main.js
+++ html/main.js
@@ -2,6 +2,91 @@ import * as THREE from "three";
 import { GLTFLoader } from "three-gltfloader";
 import { TGALoader } from "three-tgaloader";
 
+// HELPER FUNCTIONS
+
+// https://hofk.de/main/discourse.threejs/2021/RoundedRectangle/RoundedRectangle.html
+function RoundedRectangle( w, h, r, s ) { // width, height, radius corner, smoothness
+		
+	// helper const's
+	const wi = w / 2 - r;		// inner width
+	const hi = h / 2 - r;		// inner height
+	const w2 = w / 2;			// half width
+	const h2 = h / 2;			// half height
+	const ul = r / w;			// u left
+	const ur = ( w - r ) / w;	// u right
+	const vl = r / h;			// v low
+	const vh = ( h - r ) / h;	// v high	
+	
+	let positions = [
+	
+		 wi, hi, 0, -wi, hi, 0, -wi, -hi, 0, wi, -hi, 0
+		 
+	];
+	
+	let uvs = [
+		
+		ur, vh, ul, vh, ul, vl, ur, vl
+		
+	];
+	
+	let n = [
+		
+		3 * ( s + 1 ) + 3,  3 * ( s + 1 ) + 4,  s + 4,  s + 5,
+		2 * ( s + 1 ) + 4,  2,  1,  2 * ( s + 1 ) + 3,
+		3,  4 * ( s + 1 ) + 3,  4, 0
+		
+	];
+	
+	let indices = [
+		
+		n[0], n[1], n[2],  n[0], n[2],  n[3],
+		n[4], n[5], n[6],  n[4], n[6],  n[7],
+		n[8], n[9], n[10], n[8], n[10], n[11]
+		
+	];
+	
+	let phi, cos, sin, xc, yc, uc, vc, idx;
+	
+	for ( let i = 0; i < 4; i ++ ) {
+	
+		xc = i < 1 || i > 2 ? wi : -wi;
+		yc = i < 2 ? hi : -hi;
+		
+		uc = i < 1 || i > 2 ? ur : ul;
+		vc = i < 2 ? vh : vl;
+			
+		for ( let j = 0; j <= s; j ++ ) {
+		
+			phi = Math.PI / 2  *  ( i + j / s );
+			cos = Math.cos( phi );
+			sin = Math.sin( phi );
+
+			positions.push( xc + r * cos, yc + r * sin, 0 );
+
+			uvs.push( uc + ul * cos, vc + vl * sin );
+					
+			if ( j < s ) {
+			
+				idx =  ( s + 1 ) * i + j + 4;
+				indices.push( i, idx, idx + 1 );
+				
+			}
+			
+		}
+		
+	}
+		
+	const geometry = new THREE.BufferGeometry( );
+	geometry.setIndex( new THREE.BufferAttribute( new Uint32Array( indices ), 1 ) );
+	geometry.setAttribute( 'position', new THREE.BufferAttribute( new Float32Array( positions ), 3 ) );
+	geometry.setAttribute( 'uv', new THREE.BufferAttribute( new Float32Array( uvs ), 2 ) );
+	
+	return geometry;	
+	
+}
+
+// ACTUAL CODE
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xE4EFED);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -88,7 +173,7 @@ waitForData().then(() => {
     for(let i = 0; i < positions.length; i++) {
         if(!data[i]?.icon) continue;
         tgaLoader.load("data:application/octet-stream;base64," + data[i].icon, tga => {
-            const iconPlane = new THREE.PlaneGeometry(2.2, 2.2, 1, 1);
+            const iconPlane = RoundedRectangle(2.2, 2.2, .4, 18);
             const iconMaterial = new THREE.MeshPhongMaterial({
                 "map": tga,
                 "shading": THREE.FlatShading
