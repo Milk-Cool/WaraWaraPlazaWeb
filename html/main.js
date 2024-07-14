7bd9552a06cc84609f75d5679c0ea2afdf4a2edc
Milk-Cool <milan-pl@mail.ru>
2024-07-14 09:26:15 +0300

    some? optimization

--- html/main.js
+++ html/main.js
@@ -250,6 +250,24 @@ for(let _i = 0; _i < 10; _i++) {
     angle += Math.PI / 5;
 }
 
+const cacheGLTF = {};
+const loadGLTF = (url, onDone, onProgress, onError) => {
+    if(url in cacheGLTF) onDone(cacheGLTF[url].clone());
+    gltfLoader.load(url, gltf => {
+        cacheGLTF[url] = gltf;
+        onDone(gltf);
+    }, onProgress, onError);
+}
+
+const cacheImg = {};
+const loadImg = (url, onDone, onProgress, onError) => {
+    if(url in cacheGLTF) onDone(cacheImg[url].clone());
+    imgLoader.load(url, img => {
+        cacheImg[url] = img;
+        onDone(img);
+    }, onProgress, onError);
+}
+
 /**
  * @param {THREE.Texture} img The source image
  * @param {THREE.ColorRepresentation} r The color to replace red parts with (RGB)
@@ -369,46 +387,46 @@ const colorCorrectBW = (img, b, w) => {
 const loadMii = (mii, pos, commid) => {
     miis.push({});
     const n = miis.length - 1;
-    gltfLoader.load(mii.gender ? "models/body/Female.gltf" : "models/body/Male.gltf", gltf => {
+    loadGLTF(mii.gender ? "models/body/Female.gltf" : "models/body/Male.gltf", gltf => {
         gltf.scene.position.x = pos.x;
         gltf.scene.position.y = 0;
         gltf.scene.position.z = pos.z;	
         scene.add(gltf.scene);
         const color = favoriteColors[mii.favoriteColor];
-        const material = new THREE.MeshStandardMaterial({ "color": color });
+        const material = new THREE.MeshBasicMaterial({ "color": color });
         gltf.scene.children[mii.gender ? 1 : 0].material = material;
         miis[n].body = gltf.scene;
     }, undefined, console.error);
-    gltfLoader.load(`models/head/mesh/shape_${268 + mii.faceType}.glb`, gltf => {
+    loadGLTF(`models/head/mesh/shape_${268 + mii.faceType}.glb`, gltf => {
         gltf.scene.scale.set(.008, .008, .008);
         gltf.scene.position.x = pos.x;
         gltf.scene.position.y = 1.2;
         gltf.scene.position.z = pos.z;
-        const material = new THREE.MeshStandardMaterial({ "color": skinColors[mii.skinColor] });
+        const material = new THREE.MeshBasicMaterial({ "color": skinColors[mii.skinColor] });
         for(const child of gltf.scene.children)
             child.material = material;
         scene.add(gltf.scene);
         miis[n].head = gltf.scene;
     }, undefined, console.error);
-    gltfLoader.load(`models/head/mesh/shape_${329 + mii.hairType}.glb`, gltf => {
+    loadGLTF(`models/head/mesh/shape_${329 + mii.hairType}.glb`, gltf => {
         gltf.scene.scale.set(.008, .008, .008);
         gltf.scene.position.x = pos.x;
         gltf.scene.position.y = 1.15;
         gltf.scene.position.z = pos.z;
-        const material = new THREE.MeshStandardMaterial({ "color": hairColors[mii.hairColor] });
+        const material = new THREE.MeshBasicMaterial({ "color": hairColors[mii.hairColor] });
         for(const child of gltf.scene.children)
             child.material = material;
         scene.add(gltf.scene);
         miis[n].hair = gltf.scene;
     }, undefined, console.error);
-    imgLoader.load(`models/head/tex/tex_${135 + mii.eyeType}.png`, img => {
+    loadImg(`models/head/tex/tex_${135 + mii.eyeType}.png`, img => {
         let m = 0;
         miis[n].eyes = [];
         for(let i = -0.05; i <= 0.05; i += 0.1) {
             const scale = .1 * (1 + (mii.eyeScale - 4) * 0.15);
             const multSY = 1 + (mii.eyeVerticalStretch - 3) * 0.17;
             const eyePlane = new THREE.PlaneGeometry(scale, scale * multSY, 1, 1);
-            const eyeMaterial = new THREE.MeshPhongMaterial({
+            const eyeMaterial = new THREE.MeshBasicMaterial({
                 "map": colorCorrect(img, 0, 0xffffff, eyeColors[mii.eyeColor]),
                 "shading": THREE.FlatShading,
                 "transparent": true
@@ -418,13 +436,16 @@ const loadMii = (mii, pos, commid) => {
             eyeMesh.position.y = 1.45 - (mii.eyeYPosition - 12) * 0.007;
             eyeMesh.position.z = pos.z + .2;
             eyeMesh.rotation.z = (-Math.PI / 15) * (mii.eyeRotation - 4);
-            eyeMesh.material.side = THREE.DoubleSide;
-            if(i > 0) eyeMesh.rotation.y = Math.PI;
+            // eyeMesh.material.side = THREE.DoubleSide;
+            if(i > 0) {
+                eyeMesh.rotation.y = Math.PI;
+                eyeMesh.material.side = THREE.BackSide;
+            }
             scene.add(eyeMesh);
             miis[n].eyes[m++] = eyeMesh;
         }
     }, undefined, console.error);
-    imgLoader.load(`models/head/tex/tex_${215 + mii.eyebrowType}.png`, img => {
+    loadImg(`models/head/tex/tex_${215 + mii.eyebrowType}.png`, img => {
         let m = 0;
         miis[n].eyebrows = [];
         for(let i = -0.05; i <= 0.05; i += 0.1) {
@@ -441,16 +462,19 @@ const loadMii = (mii, pos, commid) => {
             eyebrowMesh.position.y = 1.48 - (mii.eyebrowYPosition - 12) * 0.007;
             eyebrowMesh.position.z = pos.z + .2;
             eyebrowMesh.rotation.z = (-Math.PI / 15) * (mii.eyebrowRotation - 6);
-            eyebrowMesh.material.side = THREE.DoubleSide;
-            if(i > 0) eyebrowMesh.rotation.y = Math.PI;
+            // eyebrowMesh.material.side = THREE.DoubleSide;
+            if(i > 0) {
+                eyebrowMesh.rotation.y = Math.PI;
+                // eyebrowMesh.material.side = THREE.BackSide;
+            }
             scene.add(eyebrowMesh);
             miis[n].eyebrows[m++] = eyebrowMesh;
         }
     }, undefined, console.error);
-    imgLoader.load(`models/head/tex/tex_${347 + mii.noseType}.png`, img => {
+    loadImg(`models/head/tex/tex_${347 + mii.noseType}.png`, img => {
         const scale = .1 * (1 + (mii.noseScale - 5) * 0.15);
         const nosePlane = new THREE.PlaneGeometry(scale, scale, 1, 1);
-        const noseMaterial = new THREE.MeshPhongMaterial({
+        const noseMaterial = new THREE.MeshBasicMaterial({
             "map": colorCorrectBWA(img, 0, 0x000000ff),
             "shading": THREE.FlatShading,
             "transparent": true
@@ -459,15 +483,15 @@ const loadMii = (mii, pos, commid) => {
         noseMesh.position.x = pos.x;
         noseMesh.position.y = 1.4 - (mii.noseYPosition - 9) * 0.007;
         noseMesh.position.z = pos.z + .2;
-        noseMesh.material.side = THREE.DoubleSide;
+        // noseMesh.material.side = THREE.DoubleSide;
         scene.add(noseMesh);
         miis[n].nose = noseMesh;
     }, undefined, console.error);
-    imgLoader.load(`models/head/tex/tex_${289 + mii.mouthType}.png`, img => {
+    loadImg(`models/head/tex/tex_${289 + mii.mouthType}.png`, img => {
         const scale = .1 * (1 + (mii.mouthScale - 4) * 0.15);
         const multSY = 1 + (mii.mouthHorizontalStretch - 3) * 0.17;
         const mouthPlane = new THREE.PlaneGeometry(scale, scale * multSY, 1, 1);
-        const mouthMaterial = new THREE.MeshPhongMaterial({
+        const mouthMaterial = new THREE.MeshBasicMaterial({
             "map": colorCorrect(img, mouthColors[mii.mouthColor],
                 Math.floor(mouthColors[mii.mouthColor] / 65536) * 0.8 * 65536
                 + Math.floor(mouthColors[mii.mouthColor] / 256) % 256 * 0.8 * 256
@@ -480,17 +504,17 @@ const loadMii = (mii, pos, commid) => {
         mouthMesh.position.x = pos.x;
         mouthMesh.position.y = 1.3 - (mii.mouthYPosition - 13) * 0.007;
         mouthMesh.position.z = pos.z + .2;
-        mouthMesh.material.side = THREE.DoubleSide;
+        // mouthMesh.material.side = THREE.DoubleSide;
         scene.add(mouthMesh);
         miis[n].mouth = mouthMesh;
     }, undefined, console.error);
-    imgLoader.load(`models/head/tex/tex_${341 + mii.mustacheType}.png`, img => {
+    loadImg(`models/head/tex/tex_${341 + mii.mustacheType}.png`, img => {
         let m = 0;
         miis[n].mustache = [];
         for(let i = -0.05; i <= 0.05; i += 0.1) {
             const scale = .1 * (1 + (mii.mustacheScale - 4) * 0.15);
             const mustachePlane = new THREE.PlaneGeometry(scale, scale, 1, 1);
-            const mustacheMaterial = new THREE.MeshPhongMaterial({
+            const mustacheMaterial = new THREE.MeshBasicMaterial({
                 "map": colorCorrectBWA(img, 0, hairColors[mii.facialHairColor] * 0x100 + 0xff),
                 "shading": THREE.FlatShading,
                 "transparent": true
@@ -499,14 +523,17 @@ const loadMii = (mii, pos, commid) => {
             mustacheMesh.position.x = pos.x + i;
             mustacheMesh.position.y = 1.35 - (mii.mustacheYPosition - 10) * 0.007;
             mustacheMesh.position.z = pos.z + .2;
-            mustacheMesh.material.side = THREE.DoubleSide;
-            if(i > 0) mustacheMesh.rotation.y = Math.PI;
+            // mustacheMesh.material.side = THREE.DoubleSide;
+            if(i > 0) {
+                mustacheMesh.rotation.y = Math.PI;
+                // mustacheMesh.material.side = THREE.BackSide;
+            }
             scene.add(mustacheMesh);
             miis[n].mustache[m++] = mustacheMesh;
         }
     }, undefined, console.error);
     if(mii.beardType != 0) 
-        gltfLoader.load(`models/head/mesh/shape_${mii.beardType < 4 ? 1 + mii.beardType : [574, 583][mii.beardType - 4]}.glb`, gltf => {
+        loadGLTF(`models/head/mesh/shape_${mii.beardType < 4 ? 1 + mii.beardType : [574, 583][mii.beardType - 4]}.glb`, gltf => {
             gltf.scene.scale.set(.008, .008, .008);
             gltf.scene.position.x = pos.x;
             gltf.scene.position.y = 1;
@@ -518,14 +545,14 @@ const loadMii = (mii, pos, commid) => {
             scene.add(gltf.scene);
             miis[n].beard = gltf.scene;
         }, undefined, console.error);
-    imgLoader.load(`models/head/tex/tex_${267 + mii.glassesType}.png`, img => {
+        loadImg(`models/head/tex/tex_${267 + mii.glassesType}.png`, img => {
         let m = 0;
         miis[n].glasses = [];
         const sizemult = (1 + (mii.glassesScale - 2) * 0.1);
         for(let i = -0.052 * sizemult; i <= 0.052 * sizemult; i += 0.052 * sizemult * 2) {
             const scale = .1 * (1 + (mii.glassesScale - 2) * 0.15);
             const glassesPlane = new THREE.PlaneGeometry(scale, scale, 1, 1);
-            const glassesMaterial = new THREE.MeshPhongMaterial({
+            const glassesMaterial = new THREE.MeshBasicMaterial({
                 "map": colorCorrectBW(img, 0x000000, glassesColors[mii.glassesColor]),
                 "shading": THREE.FlatShading,
                 "transparent": true
@@ -534,17 +561,20 @@ const loadMii = (mii, pos, commid) => {
             glassesMesh.position.x = pos.x + i;
             glassesMesh.position.y = 1.45 - (mii.glassesYPosition - 10) * 0.007;
             glassesMesh.position.z = pos.z + .25;
-            glassesMesh.material.side = THREE.DoubleSide;
-            if(i > 0) glassesMesh.rotation.y = Math.PI;
+            // glassesMesh.material.side = THREE.DoubleSide;
+            if(i > 0) {
+                glassesMesh.rotation.y = Math.PI;
+                // glassesMesh.material.side = THREE.BackSide;
+            }
             scene.add(glassesMesh);
             miis[n].glasses[m++] = glassesMesh;
         }
     }, undefined, console.error);
     if(mii.moleEnabled)
-        imgLoader.load(`models/head/tex/tex_${288}.png`, img => {
+        loadImg(`models/head/tex/tex_${288}.png`, img => {
             const scale = .03 * (1 + (mii.moleScale - 4) * 0.15);
             const molePlane = new THREE.PlaneGeometry(scale, scale, 1, 1);
-            const moleMaterial = new THREE.MeshPhongMaterial({
+            const moleMaterial = new THREE.MeshBasicMaterial({
                 "map": colorCorrectBWA(img, 0, 0x000000ff),
                 "shading": THREE.FlatShading,
                 "transparent": true
@@ -553,14 +583,14 @@ const loadMii = (mii, pos, commid) => {
             moleMesh.position.x = pos.x + (mii.moleXPosition - 8) * 0.03;
             moleMesh.position.y = 1.35 + (mii.moleYPosition - 15) * 0.01;
             moleMesh.position.z = pos.z + .2;
-            moleMesh.material.side = THREE.DoubleSide;
+            // moleMesh.material.side = THREE.DoubleSide;
             scene.add(moleMesh);
             miis[n].mole = moleMesh;
         }, undefined, console.error);
 }
 
 for(const position of positions) {
-    gltfLoader.load("models/community.gltf", gltf => {
+    loadGLTF("models/community.gltf", gltf => {
         gltf.scene.scale.set(.4, .4, .4);
         gltf.scene.position.x = position[0];
         gltf.scene.position.y = 5;
@@ -585,7 +615,7 @@ waitForData().then(() => {
         if(!data[i]?.icon) continue;
         tgaLoader.load("data:application/octet-stream;base64," + data[i].icon, tga => {
             const iconPlane = RoundedRectangle(2.5, 2.5, .5, 18);
-            const iconMaterial = new THREE.MeshPhongMaterial({
+            const iconMaterial = new THREE.MeshBasicMaterial({
                 "map": tga,
                 "shading": THREE.FlatShading
             });
@@ -593,7 +623,7 @@ waitForData().then(() => {
             iconMesh.position.x = positions[i][0];
             iconMesh.position.y = 5;
             iconMesh.position.z = positions[i][1];
-            iconMesh.material.side = THREE.DoubleSide;
+            // iconMesh.material.side = THREE.DoubleSide;
             scene.add(iconMesh);
             icons.push(iconMesh);
         }, undefined, console.error);
@@ -629,5 +659,6 @@ renderer.setAnimationLoop(animate);
 
 window.addEventListener("resize", () => {
     camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
